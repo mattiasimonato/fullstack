462235bff23c65276fb79a5a5d5f9c303bd3cb47
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-10-25 22:38:48 +0200

    CORS added to Koa

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
-const serve = require('koa-static');
 const logger = require('koa-logger');
 const router = require('./server/router');
 const bodyParser = require('koa-bodyparser');
-
+const cors = require('koa2-cors')
 app
 .use(logger())
-.use(serve('./client'))
 .use(bodyParser())
+.use(cors())
 .use(router.routes())
 .listen(3000);
